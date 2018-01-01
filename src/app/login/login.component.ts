22fa25f762ccb27d32b06cc9819159df8bf2832e
tauqeerqau <tauqeerulhassan45@gmail.com>
2018-01-01 10:08:31 +0500

    Committing Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,7 +10,27 @@ import { Component, ViewEncapsulation } from '@angular/core';
   }
 })
 export class Login {
-  constructor() {
-
+  public Email: string = "";
+  public Password: string = "";
+  public errorMessage:string = "";
+  public router:Router;
+  constructor(public _userService:UserService, router: Router) {
+    this.router = router;
+  }
+  login() {
+    this.errorMessage = "";
+    if (this.Email != "" && this.Email !== undefined && this.Password != "" && this.Password !== undefined) {
+      this._userService.login(this.Email, this.Password).subscribe(response=>{
+        if(response.code != 200){
+          this.errorMessage = response.message;
+        }
+        else{
+          this.router.navigate(["/app/dashboard"]);
+        }
+      });
+    }
+    else{
+      this.errorMessage = "User Name or Password is Required";
+    }
   }
 }
